528b621bb1191d936ebfe1c80035ddea75ff7682
Thibaut Izard <thibaut.izard@gmail.com>
2023-11-07 22:06:01 +0100

    chore(test ffor dpeloyment): test for deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Input } from "@/components/ui/input";
 export default function Home() {
   return (
     <main>
-      <h1 className="text-6xl font-bold font-parisienne">Tout sur ma ville</h1>
+      <h1 className="text-6xl font-bold font-parisienne">Tout sur ma ville2</h1>
       <Input></Input>
     </main>
   );
