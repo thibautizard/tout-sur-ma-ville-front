a3ca6e96968feb4c9a7852fd54e99e860cf49dc5
Thibaut Izard <thibaut.izard@gmail.com>
2023-11-07 22:20:19 +0100

    chore: add postgre sql database

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,15 @@
-export default function Contact() {
-  return (<h1>Contact page</h1>)
+import { sql } from "@vercel/postgres";
+
+export default async function Contact() {
+  const { rows } = await sql`SELECT * from test`;
+  return (
+    <>
+      <h1>Contact page</h1>
+      <ul>
+        {rows.map(({ id }) => (
+          <li key={id}>{id}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
