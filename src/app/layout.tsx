8dd7f9b72e41fb5cab258065c8ed533676dc1f9f
Thibaut Izard <thibaut.izard@gmail.com>
2023-11-26 18:55:09 +0100

    feat: create tags component and some refactoring

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,19 @@
 import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google";
+import { Inter as FontSans, Bayon } from "next/font/google";
 import "./globals.css";
-import { cn } from "@utils";
+import { cn } from "@tsv/utils";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
   display: "swap"
+});
+
+const bayon = Bayon({
+  subsets: ["latin"],
+  variable: "--font-bayon",
+  display: "swap",
+  weight: "400"
 });
 
 export const metadata: Metadata = {
@@ -20,7 +27,9 @@ export default function RootLayout({ children }: { readonly children: React.Reac
       lang="fr"
       suppressHydrationWarning
     >
-      <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>{children}</body>
+      <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable, bayon.variable)}>
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,35 @@
-import { Input } from "@/components/ui/input";
+import { Input } from "@tsv/components/ui/input";
+import { Tag } from "@tsv/components/ui/tag";
 
 export default function Home() {
+  const tags = [
+    {
+      id: 1,
+      tagName: "Demographie",
+      iconLink: "",
+      color: "#025B00",
+      backgroundColor: "#C9FFC8"
+    },
+    {
+      id: 2,
+      tagName: "Economie",
+      iconLink: "",
+      color: "#FBD300",
+      backgroundColor: "#FFF3B1"
+    },
+    {
+      id: 3,
+      tagName: "Education",
+      iconLink: "",
+      color: "#C900FB",
+      backgroundColor: "#EFCBF8"
+    }
+  ];
   return (
     <main>
-      <h1 className="text-6xl font-bold font-parisienne">Tout sur ma ville2</h1>
-      <Input></Input>
+      <h1 className="text-8xl font-bold text-center font-parisienne select-none">Tout sur ma ville</h1>
+      <Input />
+      <ul className="flex w-full gap-2 my-2">{tags.map(Tag)}</ul>
     </main>
   );
 }
