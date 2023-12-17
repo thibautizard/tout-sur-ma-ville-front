e1e6e915f2cf5257961e71b0cd90da53202077df
Thibaut Izard <thibaut.izard@gmail.com>
2023-12-17 23:44:38 +0100

    Refactor Category component styling

--- src/app/Category.tsx
+++ src/app/Category.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { CategoryType } from "./categories";
-
+import clsx from "clsx";
 export const Category = ({
   category,
   handleSelectedCategories,
@@ -15,16 +15,19 @@ export const Category = ({
   };
   return (
     <li
-      className={`tag flex cursor-pointer items-center gap-1.5 px-2.5 py-1 ${category.color} ${category.background}`}
+      className={clsx("flex cursor-pointer items-center gap-1.5 px-2.5 py-1", {
+        [`${category.color} ${category.background} ${category.fill}`]:
+          category.selected,
+        "bg-gray-200 fill-gray-700 text-gray-700 opacity-60":
+          !category.selected,
+      })}
       onClick={() => handleSelectedCategories(category.id)}
       onKeyUp={handleKeyUp}
     >
       {category.iconComponent({
-        className: `${category.fill} h-3`,
+        className: `fill-inherit h-3`,
       })}
-      <span
-        className={`select-none text-sm font-bold uppercase text-${category.color} font-bayon`}
-      >
+      <span className={`select-none font-bayon text-sm font-bold uppercase`}>
         {category.tagName}
       </span>
     </li>
