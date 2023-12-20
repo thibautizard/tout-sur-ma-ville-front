1aa610d1aefc18cefe0bd764476836e87217324c
Thibaut Izard <thibaut.izard@gmail.com>
2023-12-20 23:07:18 +0100

    chore(*): update monorepo settings for web app

---
+++ apps/front/app/_categories/Category.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import clsx from "clsx";
+
+export const Category = ({
+  id,
+  name,
+  iconComponent,
+  variant,
+  selected,
+  handleSelectedCategories,
+}: {
+  id: string;
+  name: string;
+  iconComponent: ({ className }: { className: string }) => JSX.Element;
+  variant: string;
+  selected: boolean;
+  handleSelectedCategories: (id: string) => void;
+}) => {
+  const handleKeyUp = (event: any) => {
+    if (event.key === "Enter") {
+      handleSelectedCategories(id);
+    }
+  };
+
+  return (
+    <li>
+      <button
+        className={clsx(
+          "flex cursor-pointer items-center gap-1.5 px-2.5 py-1",
+          {
+            [variant]: selected,
+            "bg-gray-200 fill-gray-700 text-gray-700 opacity-60": !selected,
+          },
+        )}
+        onClick={() => handleSelectedCategories(id)}
+        onKeyUp={handleKeyUp}
+      >
+        {iconComponent({
+          className: `fill-inherit h-3`,
+        })}
+        <span className={`select-none font-bayon text-sm font-bold uppercase`}>
+          {name}
+        </span>
+      </button>
+    </li>
+  );
+};
